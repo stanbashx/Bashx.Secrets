b97f5ebc2c90b25922f41be71086ee1bdf53315f
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-25 14:58:00 +0300

    signing

---
+++ src/main/bash/signing/sha256.sh
@@ -0,0 +1,66 @@
+#!/usr/local/bin/bash
+
+if [[ $# -ne 4 ]]; then
+ echo 'Wrong arguments!' >&2; exit 1; fi
+
+SECRETS_SRC="$1"
+
+if [[ -z "${SECRETS_SRC}" ]]; then
+ echo 'No src!' >&2; exit 1
+elif [[ -L "${SECRETS_SRC}" ]]; then
+ echo "\"${SECRETS_SRC}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${SECRETS_SRC}" ]]; then
+ echo "\"${SECRETS_SRC}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${SECRETS_SRC}" ]]; then
+ echo "\"${SECRETS_SRC}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${SECRETS_SRC}" ]]; then
+ echo "\"${SECRETS_SRC}\" is empty!" >&2; exit 1
+fi
+
+SECRETS_DST="$2"
+
+if [[ -z "${SECRETS_DST}" ]]; then
+ echo 'No dst!' >&2; exit 1
+elif [[ -L "${SECRETS_DST}" ]]; then
+ echo "\"${SECRETS_DST}\" is a symlink!" >&2; exit 1
+elif [[ -e "${SECRETS_DST}" ]]; then
+ if [[ -f "${SECRETS_DST}" ]]; then
+  echo "\"${SECRETS_DST}\" exists!" >&2; exit 1
+ else
+  echo "\"${SECRETS_DST}\" is not a file!" >&2; exit 1
+ fi
+fi
+
+SECRETS_KEY="$3"
+
+if [[ -z "${SECRETS_KEY}" ]]; then
+ echo 'No key!' >&2; exit 1
+elif [[ -L "${SECRETS_KEY}" ]]; then
+ echo "\"${SECRETS_KEY}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${SECRETS_KEY}" ]]; then
+ echo "\"${SECRETS_KEY}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${SECRETS_KEY}" ]]; then
+ echo "\"${SECRETS_KEY}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${SECRETS_KEY}" ]]; then
+ echo "\"${SECRETS_KEY}\" is empty!" >&2; exit 1
+fi
+
+SECRETS_PASSWORD="$4"
+
+if [[ -z "${SECRETS_PASSWORD}" ]]; then
+ echo 'No password!' >&2; exit 1; fi
+
+openssl dgst -sha256 -sign "${SECRETS_KEY}" -passin "pass:${SECRETS_PASSWORD}" -out "${SECRETS_DST}" "${SECRETS_SRC}"
+
+if [[ $? -ne 0 ]]; then
+ echo "Signing \"${SECRETS_SRC}\" error!" >&2; exit 1; fi
+
+if [[ -L "${SECRETS_DST}" ]]; then
+ echo "\"${SECRETS_DST}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${SECRETS_DST}" ]]; then
+ echo "\"${SECRETS_DST}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${SECRETS_DST}" ]]; then
+ echo "\"${SECRETS_DST}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${SECRETS_DST}" ]]; then
+ echo "\"${SECRETS_DST}\" is empty!" >&2; exit 1
+fi
