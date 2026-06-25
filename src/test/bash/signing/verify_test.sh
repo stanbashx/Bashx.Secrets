4e866449e84a59e496c741a707460e8f44e6c4ef
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-25 18:07:21 +0300

    tests

--- src/test/bash/signing/verify_test.sh
+++ src/test/bash/signing/verify_test.sh
@@ -79,6 +79,8 @@ SECRETS_ALGORITHM=''
 . $asserts/files/equals.sh "${STDERR}" $'No key!\n'
 rm "${SECRETS_SRC}"
 
+#
+
 :> "${STDOUT}"
 :> "${STDERR}"
 SECRETS_SRC="$(mktemp)"
@@ -93,7 +95,24 @@ SECRETS_ALGORITHM=''
 . $asserts/files/equals.sh "${STDERR}" "Algorithm \"${SECRETS_ALGORITHM}\" is not supported!"$'\n'
 rm "${SECRETS_SRC}"
 
-echo 'Not implemented!'; exit 1 # todo
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+SECRETS_SRC="$(mktemp)"
+printf '%s' 'foo' > "${SECRETS_SRC}"
+SECRETS_SIGNATURE="$(mktemp)"
+openssl dgst -sha256 -sign 'src/test/res/rsa4096.key' -passin 'pass:qwe123' -out "${SECRETS_SIGNATURE}" "${SECRETS_SRC}"
+if [[ $? -ne 0 ]]; then
+ echo "Signing \"${SECRETS_SRC}\" error!" >&2; exit 1; fi
+SECRETS_KEY='src/test/res/rsa4096.pub'
+SECRETS_ALGORITHM='sha256'
+"${SCRIPT}" "${SECRETS_SRC}" "${SECRETS_SIGNATURE}" "${SECRETS_KEY}" "${SECRETS_ALGORITHM}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/strings/eq.sh "${SCRIPT}" "$?" '0'
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/empty.sh "${STDERR}"
+rm "${SECRETS_SRC}"
+rm "${SECRETS_SIGNATURE}"
 
 #
 
